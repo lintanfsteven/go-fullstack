0cb93be5ef6ae07b746423af212a12f85e44ac00
StevenL <lintanf.steven@gmail.com>
2022-06-22 23:32:47 +0200

    changes

--- routes/stuff.js
+++ routes/stuff.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const stuffCtrl = require('../controllers/stuff');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post('/', auth, stuffCtrl.createThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
 
-router.put('/:id', auth, stuffCtrl.modifyThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 
 router.delete('/:id', auth, stuffCtrl.deleteThing);
 
